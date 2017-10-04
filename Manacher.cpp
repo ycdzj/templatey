f87461d19b70bdaa79d6ff607f337a84e87fb132
dudu321 <dudu321@surface_pc>
2017-10-04 21:36:59 +0800

    initial commit; add Manacher and ExKmp

---
+++ Manacher.cpp
@@ -0,0 +1,10 @@
+void Manacher(char *str, int len, int p[]) {
+	p[0] = 1;
+	int key = 0;
+	for(int i = 1; i < len; i++) {
+		int k = (key + p[key] == i) ? 1 : min(key + p[key] - i, p[2 * key - i]);
+		while(i - k >= 0 && i + k < len && str[i + k] == str[i - k]) k++;
+		p[i] = k;
+		if(i + p[i] > key + p[key]) key = i;
+	}
+}
---
+++ exKMP.cpp
@@ -0,0 +1,35 @@
+/*
+getExNext: 以pat为模式串计算扩展KMP的next数组
+
+exKmpMatch：以pat为模式串对s进行匹配。next是pat的next数组，最终结果保存在lcp中
+
+*/
+void getExNext(string &pat, vector<int> &next) {
+	int len = pat.length();
+	next.resize(len);
+
+	next[0] = len;
+	for(int a = 1, p = 1, i = 1; i < len; i++) {
+		if(p <= i) a = p = i;
+
+		int k = min(next[i - a], p - i);
+		while(i + k < len && pat[i + k] == pat[k]) k++;
+		next[i] = k;
+
+		if(i + k > p) p = i + k, a = i;
+	}
+}
+void exKmpMatch(string &s, string &pat, vector<int> &next, vector<int> &lcp) {
+	//getExNext(pat, next);
+	int len_s = s.length(), len_p = pat.length();
+	lcp.resize(len_s);
+	for(int a = 0, p = 0, i = 0; i < len_s; i++) {
+		if(p <= i) a = p = i;
+
+		int k = min(next[i - a], p - i);
+		while(k < len_p && i + k < len_s && pat[k] == s[i + k]) k++;
+		lcp[i] = k;
+
+		if(i + k > p) p = i + k, a = i;
+	}
+}
