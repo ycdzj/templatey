75f5d330a00088a6f8ff0cd3d7c991a12594dd2d
dudu321 <dudu321@surface_pc>
2017-10-16 12:15:49 +0800

    add checker and t~e~x~p~l~a~t~e~

---
+++ checker.sh
@@ -0,0 +1,33 @@
+echo "compiling..."
+if !(g++ gen.cpp -o gen.out && g++ code1.cpp -o code1.out && g++ code2.cpp -o code2.out)
+then
+        echo "compilation failed, program exiting..."
+        exit
+fi
+
+printf "enter number of attempts:"
+read N
+
+i=1
+while [ $i -le $N ];
+do
+        FILENAME=attempt_${i}.txt
+        printf "attempt `echo $i`: generating..."
+        ./gen.out > $FILENAME
+        printf "code1 running..."
+        ./code1.out < $FILENAME > code1_output.txt
+        printf "code2 running..."
+        ./code2.out < $FILENAME > code2_output.txt
+
+        if diff code1_output.txt code2_output.txt
+        then
+                printf "passed.\n"
+                rm $FILENAME
+        else
+                printf "OUTPUT NOT IDENTICAL!\n"
+        fi
+
+        i=$((i+1))
+done
+
+rm code1_output.txt code2_output.txt
