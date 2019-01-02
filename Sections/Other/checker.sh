1d40ea0c6885cc597ec87922a82c869ea44ca517
ycdzj <yczhao0123@gmail.com>
2019-01-02 15:37:30 +0800

    rewrite checker.sh

--- Sections/Other/checker.sh
+++ Sections/Other/checker.sh
@@ -1,4 +1,4 @@
-if !(g++ g.cpp -o g.out && g++ --std=c++11 c1.cpp -o c1.out && g++ c2.cpp -o c2.out)
+if !(g++ g.cpp -o g && g++ --std=c++11 c1.cpp -o c1 && g++ c2.cpp -o c2)
 then
 	exit
 fi
@@ -6,19 +6,17 @@ fi
 echo read
 read N
 
-RF=rand.txt
 i=1
 while [ $i -le $N ];
 do
-	FN=a${i}.txt
-	echo $i > $RF
-	./g.out < $RF > $FN
-	./c1.out < $FN > c1.txt
-	./c2.out < $FN > c2.txt
-	if diff c1.txt c2.txt
+    echo $i
+	echo $i | ./g | ./c1 > a
+	echo $i | ./g | ./c2 > b
+	if ! diff a b
 	then
-		echo pass
-		rm $FN
+		echo $i | ./g > $i.txt
+        cat a >> $i.txt
+        cat b >> $i.txt
 	fi
 	i=$((i+1))
 done
