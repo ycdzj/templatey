ad34ee27ec242ac7ef24d387d25df51ee115a67b
ycdzj <yczhao0123@gmail.com>
2019-08-28 00:38:15 +0800

    add 线性素数筛和欧拉函数

--- Sections/Math/prime.cpp
+++ Sections/Math/prime.cpp
@@ -1,6 +1,26 @@
-void prime(int a[], int n) {
-	for(int i = 1; i <= n; i++) a[i] = i;
-	for(int i = 2; i * i <= n; i++) if(a[i] == i) {
-		for(int j = i * i; j <= n; j += i) a[j] = min(a[j], i);
+bool vis[MAXN];
+int pri[MAXN], cnt_pri;
+int phi[MAXN];
+
+void prime() {
+	phi[1] = 1;
+	for(int i = 2; i < MAXN; i++) {
+		if(!vis[i]) {
+			pri[cnt_pri++] = i;
+			phi[i] = i - 1;
+		}
+		for(int j = 0; j < cnt_pri; j++) {
+			if((long long)i * pri[j] >= MAXN) {
+				break;
+			}
+			vis[i * pri[j]] = true;
+			if(i % pri[j] != 0) {
+				phi[i * pri[j]] = phi[i] * phi[pri[j]];
+			}
+			else {
+				phi[i * pri[j]] = pri[j] * phi[i];
+				break;
+			}
+		}
 	}
 }
